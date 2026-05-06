9d2e40d0a3d57bd5c153f1d27d0858fefa7ca9e3
hddgpp <youssefhehe3@gmail.com>
2026-05-07 00:04:52 +0200

    switch to brevo smtp

--- src/email.js
+++ src/email.js
@@ -2,7 +2,8 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp-relay.brevo.com',
+  port: 587,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
